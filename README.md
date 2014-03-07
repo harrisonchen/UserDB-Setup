dd76e99e7a55d95738f06d90dd76f12a6157394a
Harrison Chen <harrisonxchen@gmail.com>
2014-03-06 17:39:54 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Rails User Database Setup with SQLite
+=====================================
+
+An easy setup for a users database table on Rails 4.0 with SQLite.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserDBSetup::Application.config.secret_key_base = '6a98056c20c4431f6c0686382e69b91368023374de9e61f3a3cf6097574ceaeaad1715f278f07370ff268289a929067ad0b01a33aeb4d66cfba42838ca4a7264'

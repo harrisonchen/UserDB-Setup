9dd647f378ff60f84825972556c15b6895fb3ffc
Harrison Chen <harrisonxchen@gmail.com>
2014-03-07 00:29:12 -0800

    Improve layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ UserDBSetup::Application.routes.draw do
   resources :users
 
   root 'users#index'
+
+  match '/signup', to: 'users#new', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
